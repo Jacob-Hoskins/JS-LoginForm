f0e5f7e0b21f2891bf6c2210e150f9560ee87bb8
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2021-05-05 07:39:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+	e.preventDefault();
+	console.log(nameInput.value);
+
+
+	if(nameInput.value === '' || emailInput.value === ''){
+		alert("Please enter field")
+	}else{
+		console.log('successs');
+	}
+}
